974f89a4fa6732dec4f6de69114863e138a3453e
hiroki <hiroki@homepc>
2020-09-25 23:39:45 +0900

    PDFの比較方法を変更

--- converter/src/functions/PDFManager.java
+++ converter/src/functions/PDFManager.java
@@ -8,6 +8,7 @@ import java.io.OutputStream;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.util.ArrayList;
 
 import javax.imageio.ImageIO;
 
@@ -60,14 +61,14 @@ public class PDFManager {
 	}
 
 	/**
-	 * @brief	2つのPDFを比較して、比較結果をページ単位でpngで出力する
+	 * @brief	2つのPDFをビジュアルで比較して、比較結果をページ単位でpngで出力する
 	 * @param	基準となるPDFファイル
 	 * @param	比較対象のPDFファイル
 	 * @param	比較結果を出力するフォルダのパス
-	 * @return	2つのPDFファイルが一致するかどうか
+	 * @return	2つのPDFファイルの比較結果
 	 */
-	public static boolean comparePDF(PDFInfo basePDF, PDFInfo compPDF, String dir) {
-		boolean isMatched = true;
+	public static String comparePDF(PDFInfo basePDF, PDFInfo compPDF, String dir) {
+		StringBuilder comparedResult = new StringBuilder(basePDF.getFileName() + " Result: ");
 
 		String baseFilePath = basePDF.getFileFullPath();
 		String compFilePath = compPDF.getFileFullPath();
@@ -85,24 +86,63 @@ public class PDFManager {
 			int basePageNumber = basePDF.getPageNumber();
 			int compPageNumber = compPDF.getPageNumber();
 
-			if (basePageNumber != compPageNumber) {
-				return false;
-			}
+			boolean isSameAppearance = true;
+			if (basePageNumber != compPageNumber) isSameAppearance = false;
 
-			for(int i = 0; i < basePageNumber; ++i) {
+			int minPageNumber = Math.min(basePageNumber, compPageNumber);
+			for(int i = 0; i < minPageNumber; ++i) {
 				BufferedImage baseImg = baseRend.renderImageWithDPI(i, 144, ImageType.RGB);
 				BufferedImage compImg = compRend.renderImageWithDPI(i, 144, ImageType.RGB);
 
 				Path resultDir = Paths.get(dir + "\\diff-" + fileName + i + ".png");
 
 				try (OutputStream os = Files.newOutputStream(resultDir)) {
-					isMatched = compareImage(baseImg,compImg, os);
+
+					if (isSameAppearance) {
+						isSameAppearance = compareImage(baseImg, compImg,os);
+					} else {
+						compareImage(baseImg, compImg, os);
+					}
 				}
+			}
+
+			if (isSameAppearance) {
+				comparedResult.append("Same.\n");
+			} else {
+				comparedResult.append("Different.\n");
 			}
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
 
-		return isMatched;
+		return comparedResult.toString();
+	}
+
+	/**
+	 * @brief	2つのPDFリストをビジュアルで比較して、比較結果をページ単位でpngで出力する
+	 * @param	基準となるPDFファイルのリスト
+	 * @param	比較対象のPDFファイルのリスト
+	 * @param	比較結果を出力するフォルダのパス
+	 * @return	各PDFファイルの結果
+	 */
+	public static String comparePDF(ArrayList<PDFInfo> basePDFs, ArrayList<PDFInfo> compPDFs, String dir) {
+		StringBuilder comparedResults = new StringBuilder();
+
+		int baseFileNumbers = basePDFs.size();
+		int compFileNumbers = compPDFs.size();
+
+		for(int baseCounter = 0; baseCounter < baseFileNumbers; ++baseCounter) {
+			for(int compCounter = 0; compCounter < compFileNumbers; ++compCounter) {
+				String baseFileName = basePDFs.get(baseCounter).getFileName();
+				String compFileName = compPDFs.get(compCounter).getFileName();
+
+				if (baseFileName == null) continue;
+				if (!baseFileName.equals(compFileName)) continue;
+
+				comparedResults.append(comparePDF(basePDFs.get(baseCounter), compPDFs.get(compCounter), dir));
+			}
+		}
+
+		return comparedResults.toString();
 	}
 }
